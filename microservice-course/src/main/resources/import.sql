4dbf78cc229968bbdf8f31ab77d6cb04727c998d
Diogenes <albert123ot@gmail.com>
2024-03-16 16:19:17 -0500

    BIENVENIDA

--- microservice-course/src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO courses (name,teacher) VALUES ('Matemáticas','Profesor A');
-INSERT INTO courses (name,teacher) VALUES ('Comunicación','Profesor B');
-INSERT INTO courses (name,teacher) VALUES ('Algebra','Profesor C');
-INSERT INTO courses (name,teacher) VALUES ('Religión','Profesor D');
-INSERT INTO courses (name,teacher) VALUES ('Geometría','Profesor E');
-INSERT INTO courses (name,teacher) VALUES ('Psicologia','Profesor F');
-INSERT INTO courses (name,teacher) VALUES ('Educacion Fisica','Profesor G');
-INSERT INTO courses (name,teacher) VALUES ('Historia','Profesor H');
