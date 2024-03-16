92847837afc16e80593fa834b77c28865a655eb6
Diogenes <albert123ot@gmail.com>
2024-03-16 16:06:35 -0500

    Microservicios

---
+++ microservice-student/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Juan', 'Perez','juan.perez@example.com',1);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Maria', 'Santa Maria','maria.perez@example.com',2);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Marcos', 'Aguirre','marcos.perez@example.com',1);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Lucas', 'Zapallal','lucas.perez@example.com',3);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Pedro', 'Espinoza','pedro.perez@example.com',2);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Jaren', 'Palacios','jaren.perez@example.com',1);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Devora', 'Rosa','devora.perez@example.com',3);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Junior', 'Paredes','junior.perez@example.com',2);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Frank', 'Espinal','frank.perez@example.com',3);
+INSERT INTO students (name,last_name,email,course_id) VALUES ('Diego', 'Zambrano','diego.perez@example.com',2);
